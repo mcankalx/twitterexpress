eb914e45ec009b2eccc4032958f5cbdfe88eb579
mcankalx <mantoo_9@rediffmail.com>
2021-05-21 03:56:55 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+var OAuth = require('OAuth');
+
+
+
 // Load request module
 var request = require('request');
 
@@ -22,34 +26,44 @@ const finalConfig = lodash.merge(defaultConfig, environmentConfig);
 // as a best practice all global variables should be referenced via global syntax and their names should always begin with g
 global.gConfig = finalConfig;
 
+var oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  global.gConfig.oauth_twitterKey,
+  global.gConfig.oauth_twitterSecret,
+  '1.0A',
+  null,
+  'HMAC-SHA1'
+);
+
+
 
 // this function routes the HTTP GET Requests to the provide URL path which is being specified with the specified access token and access keys
 app.get(global.gConfig.app_uri, function(req, res) {
   // Reading the tweet id passed in the request URL
 	var tweet_id = req.params['id'];
-        console.log("consumer key  : "+global.gConfig.oauth_consumer_key);
-	// setting the http URL, authorization details
-    var options = {
-        'method': 'GET',
-        'url': global.gConfig.url_twitter + tweet_id + '?tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld',
-        'headers': {
-            'Authorization': 'OAuth oauth_consumer_key='+global.gConfig.oauth_consumer_key+',oauth_token='+global.gConfig.oauth_token+',oauth_signature_method='+global.gConfig.oauth_signature_method+',oauth_timestamp="1621533888",oauth_nonce='+global.gConfig.oauth_nonce+',oauth_version='+global.gConfig.oauth_version+',oauth_signature='+global.gConfig.oauth_signature
-  }
-    };
-    request(options, function(error, response) {
-        if (error) throw new Error(error);
-        // logs the resonse message body to command prompt console
-		console.log(response.body);
-		// logs the resonse message body to command prompt console
-		res.end(response.body);
-    });
+       oauth.get(
+  global.gConfig.url_twitter + tweet_id + '&tweet.fields=attachments,author_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld',
+    global.gConfig.oauth_token,
+  global.gConfig.oauth_secret,
+  function (error, data, response){
+    if (error) console.error(error);
+    data = JSON.parse(data);
+    // console.log(JSON.stringify(data, 0, 2));
+    // console.log(response.body);
+   console.log(JSON.stringify(data, 0, 2));
+   		res.end(JSON.stringify(data, 0, 2));
+
+
+}); 
+	
 });
 
 // setting the application to listen on port configured in the config file
 var server = app.listen(global.gConfig.node_port, function() {
     var host = server.address().address
     var port = server.address().port
-    console.log("Fetch Tweet Details app listening at http://%s:%s", host, port)
+    console.log("Fetch Tweet Details app listening at %s", port)
 
 })
 
